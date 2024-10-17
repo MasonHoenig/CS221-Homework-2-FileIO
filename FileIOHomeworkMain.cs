999f82cbe9337aad5429142d33161fd651a09536
Mason Hoenig <masonhoenig@gmail.com>
2024-10-17 13:21:20 -0500

    Add project files.

---
+++ FileIOHomeworkMain.cs
@@ -0,0 +1,114 @@
+﻿namespace Homework2
+{
+    class FileIOHomework
+    {
+        private static void Main()
+        {
+            const string path = "NFL-2018-Standings.csv";
+            int LineCount = GetLineCount(path);
+
+            Standings[] standings = new Standings[LineCount - 1];
+
+            try
+            {
+                using StreamReader reader = new StreamReader(path);
+
+                reader.ReadLine();
+
+                for (int i = 0; i < LineCount - 1; i++)
+                {
+                    string Line = reader.ReadLine();
+                    string[] cols = Line.Split(",");
+
+                    string team = cols[0];
+                    int wins = int.Parse(cols[1]);
+                    int losses = int.Parse(cols[2]);
+                    int ties = int.Parse(cols[3]);
+                    double winLossPercentage = double.Parse(cols[4]);
+                    int pointsFor = int.Parse(cols[5]);
+                    int pointsAgainst = int.Parse(cols[6]);
+                    int pointsDiff = int.Parse(cols[7]);
+                    double marginOfVictoy = double.Parse(cols[8]);
+                    bool wonDivision = bool.Parse(cols[9]);
+                    bool wildCard = bool.Parse(cols[10]);
+
+                    standings[i] = new Standings(team, wins, losses, ties, winLossPercentage, pointsFor, pointsAgainst, pointsDiff, marginOfVictoy, wonDivision, wildCard);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error reading from file: {e.Message}");
+            }
+
+            SortData(standings);
+
+            Console.WriteLine("Reading from CVS file...");
+            Console.WriteLine("Done!");
+            Console.WriteLine();
+            Console.WriteLine("Sorting data...");
+            Console.WriteLine("Done!");
+            Console.WriteLine();
+
+            WriteToConsole(standings);
+
+            Console.WriteLine();
+            Console.WriteLine("Writing data to CSV file...");
+            Console.WriteLine("Done!");
+
+            WriteToCSVFile(standings, LineCount);
+        }
+        private static int GetLineCount(string path)
+        {
+            using StreamReader reader = new StreamReader(path);
+
+            int lines = 0;
+            while (!reader.EndOfStream)
+            {
+                reader.ReadLine();
+                lines++;
+            }
+            return lines;
+        }
+        private static void SortData(Standings[] standings)
+        {
+            Standings temp;
+
+            for (int i = 1; i < standings.Length; i++)
+            {
+                int j = i;
+                temp = standings[j];
+
+                while (j > 0 && temp.Wins > standings[j - 1].Wins)
+                {
+                    standings[j] = standings[j - 1];
+                    j--;
+                }
+                standings[j] = temp;
+            }
+        }
+        private static void WriteToCSVFile(Standings[] standings, int linecount)
+        {
+            string FilePath = @"2018-NFL-Standings-Sorted.csv";
+            using StreamWriter Writer = new StreamWriter(FilePath);
+            try
+            {
+                Writer.WriteLine("Team, Wins, Losses, Ties, Win/Loss Percentage, Points For, Points Against, Points Differencial, Margin of Victory, Won Division, Wildcard");
+                for (int i = 0; i < linecount - 1; i++)
+                {
+                    Writer.WriteLine(standings[i].ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        private static void WriteToConsole(Standings[] standings)
+        {
+            for (int i = 0; i < standings.Length; i++)
+            {
+                Console.WriteLine(standings[i].ConsoleToString());
+            }
+        }
+    }
+}
---
+++ Standings.cs
@@ -0,0 +1,158 @@
+﻿namespace Homework2
+{
+    class Standings
+    {
+        private string team;
+        private int wins;
+        private int losses;
+        private int ties;
+        private double winLossPercentage;
+        private int pointsFor;
+        private int pointsAgainst;
+        private int pointsDiff;
+        private double margingOfVictory;
+        private bool wonDivision;
+        private bool wildcard;
+
+        public Standings(string team , int wins, int losses, int ties, double winLossPercentage, int pointsFor, int pointsAgainst, int pointsDiff, double margingOfVictory, bool wonDivision, bool wildcard)
+        {
+            Team = team;
+            Wins = wins;
+            Losses = losses;
+            Ties = ties;
+            WinLossPercentage = winLossPercentage;
+            PointsFor = pointsFor;
+            PointsAgainst = pointsAgainst;
+            PointsDiff = pointsDiff;
+            MarginOfVictory = margingOfVictory;
+            WonDivision = wonDivision;
+            WildCard = wildcard;
+        }
+
+        public override string ToString()
+        {
+            return $"{team}, {wins}, {losses}, {ties}, {winLossPercentage}, {pointsFor}, {pointsAgainst}, {pointsDiff}, {margingOfVictory}, {wonDivision}, {wildcard}";
+        }
+
+        public string ConsoleToString()
+        {
+            return
+                $"{team}, Wins: {wins}, Losses: {losses}, Ties: {ties}, Win/Loss Percentage: {winLossPercentage}, " +
+                $"Points For: {pointsFor}, Points Agianst {pointsAgainst}, Point Differencial {pointsDiff}, " +
+                $"Margin of Victory: {margingOfVictory}, Won Division: {wonDivision}, Wildcard Appearence: {wildcard}";
+        }
+
+        private string Team
+        {
+            get => team;
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Invalid Name");
+                }
+                team = value;
+            }
+        }
+        //is using public property with private set valid
+        public int Wins
+        {
+            get => wins;
+            private set
+            {
+                if (value > 16 || value < 0)
+                {
+                    throw new Exception("Invalid number of wins");
+                }
+                wins = value;
+            }
+        }
+        private int Losses
+        {
+            get => losses;
+            set
+            {
+                if (value > 16 || value < 0)
+                {
+                    throw new Exception("Invalid number of losses");
+                }
+                losses = value;
+            }
+        }
+        private int Ties
+        {
+            get => ties;
+            set
+            {
+                if (value > 16 || value < 0)
+                {
+                    throw new Exception("Invalid number of ties");
+                }
+                ties = value;
+            }
+        }
+        private double WinLossPercentage
+        {
+            get => winLossPercentage;
+            set
+            {
+                if(value > 1 || value < 0)
+                {
+                    throw new Exception("Invalid Win/Loss Percentage");
+                }
+                winLossPercentage = value;
+            }
+        }
+        private int PointsFor
+        {
+            get => pointsFor;
+            set
+            {
+                if (value > 2000 || value < 0)
+                {
+                    throw new Exception("Invalid points for value");
+                }
+                pointsFor = value;
+            }
+        }
+        private int PointsAgainst
+        {
+            get => pointsAgainst;
+            set
+            {
+                if (value > 2000 || value < 0)
+                {
+                    throw new Exception("Invalid points against value");
+                }
+                pointsAgainst = value;
+            }
+        }
+        private int PointsDiff
+        {
+            get => pointsDiff;
+            set
+            {
+                if(value > 10000 || value < -1000)
+                {
+                    throw new Exception("Invalid point differencial value");
+                }
+                pointsDiff = value;
+            }
+        }
+        private double MarginOfVictory
+        {
+            get => margingOfVictory;
+            set
+            {
+                if (value > 100 || value < -100)
+                {
+                    throw new Exception("invalid margin of victory value");
+                }
+                margingOfVictory = value;
+            }
+        }
+        private bool WonDivision { get; set; }
+
+        private bool WildCard { get; set; }
+    }
+}
